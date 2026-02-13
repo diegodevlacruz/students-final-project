53f0d7d27062cf61f260f044c1d84eb2d59ea842
Diego de la Cruz <diego.dlacruz@gmail.com>
2026-02-13 13:17:32 -0600

    Initial Commit

---
+++ src/main/java/com/diegodevlacruz/finalproject/StudentsFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.diegodevlacruz.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diegodevlacruz/finalproject/StudentsFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegodevlacruz.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
